217612850cdeb233f38ca10aa5863631ac8bd23b
Hikmot28 <hikmotsanni@gmail.com>
2025-06-13 18:05:48 +0100

    Update practise.py

--- practise.py
+++ practise.py
@@ -2,10 +2,13 @@ import pandas as pd
 import streamlit as st
 import matplotlib.pyplot as pt
 import numpy as np
-
+import seaborn as sns
 
 
 
 #read a csv file
 #import csv file
 #convert file to dataframe
+
+df = pd.read_csv("diabetes.csv")
+st.write(df.head())
