957393c780571c37b4824767ee27af9e10250e62
Hikmot28 <hikmotsanni@gmail.com>
2025-06-13 17:51:38 +0100

    Update practise.py

---
+++ practise.py
@@ -0,0 +1,11 @@
+import pandas as pd 
+import streamlit as st
+import matplotlib.pyplot as pt
+import numpy as np
+import seaborn as sns
+
+
+
+#read a csv file
+#import csv file
+#convert file to dataframe
